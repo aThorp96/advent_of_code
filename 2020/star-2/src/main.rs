833b3866748b00a9b1245315fbfc7ad804859b8f
Andrew Thorp <andrew.thorp.dev@gmail.com>
2020-12-02 17:19:59 -0500

    Complete star 2

---
+++ 2020/star-2/src/main.rs
@@ -0,0 +1,54 @@
+use std::path::Path;
+use std::fs::File;
+use std::io::BufReader;
+use std::io::prelude::*;
+use std::error::Error;
+
+use std::collections::BTreeSet;
+
+fn main() {
+	let mut nums: BTreeSet<usize> = BTreeSet::new();
+	let mut added: BTreeSet<usize> = BTreeSet::new();
+	let mut o = 0;
+
+    let path = Path::new("input.txt");
+    let file = match File::open(&path) {
+        Ok(f) => f,
+        Err(e) => panic!("Error opening file: {}", Error::to_string(&e)),
+    };
+
+    let reader = BufReader::new(file);
+	let lines = reader.lines();
+
+	// O(n)
+	for l in lines {
+    	let i = l.unwrap().parse::<usize>().unwrap();
+    	nums.insert(i);
+	};
+
+	// O(n*n*n)
+	for i in nums.range(0..2021) {
+    	for j in nums.range(0..(2021-i)) {
+        	// let k = 2020 - i - j;
+        	// if nums.contains(&k) {
+            // 	println!("{} + {} + {} = {}", i, j, k, i + j + k);
+            // 	println!("{} * {} * {} = {}", i, j, k, i * j * k);
+            // 	println!("o: {}", o);
+            // 	return
+        	// }
+        	for k in nums.range(0..(2021-i-j)) {
+            	o = o + 1;
+            	if i != j && i != k && j != k {
+                	// If the current number's counterpart has been found, print the result
+                    // and short circuit
+                	if i + j + k == 2020 {
+                    	println!("{} + {} + {} = {}", i, j, k, i + j + k);
+                    	println!("{} * {} * {} = {}", i, j, k, i * j * k);
+                    	println!("o: {}", o);
+                    	return
+                	}
+            	}
+        	}
+    	}
+	};
+}
