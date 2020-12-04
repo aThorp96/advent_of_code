18bca87d64cbdf228509c63d64cf76fc6019d25a
Andrew Thorp <andrew.thorp.dev@gmail.com>
2020-12-03 22:43:25 -0500

    Fix clippy warnings

--- 2020/star-4/src/main.rs
+++ 2020/star-4/src/main.rs
@@ -1,14 +1,15 @@
-/// Each line gives the password policy and then the password. The password 
-/// policy indicates the lowest and highest number of times a given letter must
-/// appear for the password to be valid. For example, `1-3 a` means that the password
-/// must contain a at least 1 time and at most 3 times.
-/// `1-3 a: ajaka`
+/// Each line gives the password policy and then the password. The password
+/// policy indicates the two indices the  given letter must appear exacly once in
+/// for the password to be valid (1 indexed). For example, `1-3 a` means that the password
+/// must contain 'a' at index 1 exclusive or at index 3.
+/// `2-3 a: ajaka` // valid
+/// `1-3 a: ajaka` // invalid
+use std::error::Error;
+use std::fs::File;
+use std::io::prelude::*;
+use std::io::BufReader;
 
 use std::path::Path;
-use std::fs::File;
-use std::io::BufReader;
-use std::io::prelude::*;
-use std::error::Error;
 use std::string::String;
 
 struct PasswordRule {
@@ -20,37 +21,35 @@ struct PasswordRule {
 impl PasswordRule {
     fn new(rule_str: &str) -> PasswordRule {
         // Get delemeters
-        let d1 = rule_str.find("-").unwrap();
-        let d2 = rule_str.find(" ").unwrap();
+        let d1 = rule_str.find('-').unwrap();
+        let d2 = rule_str.find(' ').unwrap();
 
-        PasswordRule{
+        PasswordRule {
             p1: rule_str[..d1].parse::<usize>().unwrap(),
-            p2: rule_str[d1+1..d2].parse::<usize>().unwrap(),
-            letter: rule_str.as_bytes()[d2+1] as char,
+            p2: rule_str[d1 + 1..d2].parse::<usize>().unwrap(),
+            letter: rule_str.as_bytes()[d2 + 1] as char,
         }
     }
 
     fn matches(&self, pw: &str) -> bool {
-        let c1 = pw.as_bytes()[self.p1-1] as char;
-        let c2 = pw.as_bytes()[self.p2-1] as char;
-        println!("c1: {}\tc2: {}", c1, c2);
-		!(c1 == self.letter && c2 == self.letter) && (c1 == self.letter || c2 == self.letter)
+        let c1 = pw.as_bytes()[self.p1 - 1] as char;
+        let c2 = pw.as_bytes()[self.p2 - 1] as char;
+        !(c1 == self.letter && c2 == self.letter) && (c1 == self.letter || c2 == self.letter)
     }
 }
 
 fn validate_data_line(data_line: String) -> bool {
-	let delemeter = data_line.find(":").unwrap();
-	let rule_str = &data_line[..delemeter];
-	let phrase_str = &data_line[delemeter+2..];
+    let delemeter = data_line.find(':').unwrap();
+    let rule_str = &data_line[..delemeter];
+    let phrase_str = &data_line[delemeter + 2..];
 
-	// This could simply be returned, but I'd like to see the results to validate
-	let valid = PasswordRule::new(rule_str).matches(phrase_str);
-	if !valid {
-    	println!("Invalid pw found: {}", data_line);
-	}
-	valid
+    // This could simply be returned, but I'd like to see the results to validate
+    let valid = PasswordRule::new(rule_str).matches(phrase_str);
+    if !valid {
+        println!("Invalid pw found: {}", data_line);
+    }
+    valid
 }
-
 
 fn main() {
     let path = Path::new("input.txt");
@@ -60,17 +59,15 @@ fn main() {
     };
 
     let reader = BufReader::new(file);
-	let lines = reader.lines();
-	let mut num_valid = 0;
+    let lines = reader.lines();
+    let mut num_valid = 0;
 
-	// O(n)
-	for l in lines {
-    	let d = l.unwrap();
-    	if validate_data_line(d) {
-        	num_valid = num_valid + 1;
-    	}
-	};
+    for l in lines {
+        let d = l.unwrap();
+        if validate_data_line(d) {
+            num_valid += 1;
+        }
+    }
 
-	println!("Num valid: {}", num_valid);
-
+    println!("Num valid: {}", num_valid);
 }
